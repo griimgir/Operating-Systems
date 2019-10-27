af97df4910a39d61fbaeeb0f0b4688fbda34793f
griimgir <griimgir@gmail.com>
2019-10-27 14:24:53 -0700

    update Sunday 10/27

--- nachos/threads/Alarm.java
+++ nachos/threads/Alarm.java
@@ -1,53 +1,89 @@
 package nachos.threads;
 
 import nachos.machine.*;
-
+import java.util.TreeSet;
 /**
  * Uses the hardware timer to provide preemption, and to allow threads to sleep
  * until a certain time.
  */
 public class Alarm {
-    /**
-     * Allocate a new Alarm. Set the machine's timer interrupt handler to this
-     * alarm's callback.
-     *
-     * <p><b>Note</b>: Nachos will not function correctly with more than one
-     * alarm.
-     */
-    public Alarm() {
-	Machine.timer().setInterruptHandler(new Runnable() {
-		public void run() { timerInterrupt(); }
-	    });
-    }
+	// CREATING A PRIVATE TREESET THAT CAN BE ONLY ACCESSES W/IN THIS CLASS
+	// TREESET BECAUSE IT'S AN ASCENDING SET, IMPLYING IT IS ALREADY SORTED 
+	// AND UNIQUE
+	private TreeSet<KThread> standbyQueue = new TreeSet<KThread>();
+	
+	// INITIALIZING wakeTime TO BE USED IN THE timerInterrupt AND waitUntil METHOD
+	private long wakeTime = 0L;
 
-    /**
-     * The timer interrupt handler. This is called by the machine's timer
-     * periodically (approximately every 500 clock ticks). Causes the current
-     * thread to yield, forcing a context switch if there is another thread
-     * that should be run.
-     */
-    public void timerInterrupt() {
-	KThread.currentThread().yield();
-    }
+	/**
+	 * Allocate a new Alarm. Set the machine's timer interrupt handler to this
+	 * alarm's callback.
+	 *
+	 * <p><b>Note</b>: Nachos will not function correctly with more than one
+	 * alarm.
+	 */
 
-    /**
-     * Put the current thread to sleep for at least <i>x</i> ticks,
-     * waking it up in the timer interrupt handler. The thread must be
-     * woken up (placed in the scheduler ready set) during the first timer
-     * interrupt where
-     *
-     * <p><blockquote>
-     * (current time) >= (WaitUntil called time)+(x)
-     * </blockquote>
-     *
-     * @param	x	the minimum number of clock ticks to wait.
-     *
-     * @see	nachos.machine.Timer#getTime()
-     */
-    public void waitUntil(long x) {
-	// for now, cheat just to get something working (busy waiting is bad)
-	long wakeTime = Machine.timer().getTime() + x;
-	while (wakeTime > Machine.timer().getTime())
-	    KThread.yield();
-    }
+	public Alarm() {
+		Machine.timer().setInterruptHandler(new Runnable() {
+			public void run() { timerInterrupt(); }
+		});
+	}
+
+	/**
+	 * The timer interrupt handler. This is called by the machine's timer
+	 * periodically (approximately every 500 clock ticks). Causes the current
+	 * thread to yield, forcing a context switch if there is another thread
+	 * that should be run.
+	 */
+	public void timerInterrupt() {
+		
+		// if ( standbyQueue is not empty){
+		if(!standbyQueue.isEmpty())									
+		{
+		//  systemCurrentTime = read time from system;
+			long currentSystemTime = Machine.timer().getTime();		
+			
+			// while ( systemCurrentTime is at least wakeUpTime){
+			while (currentSystemTime >= wakeTime)		
+
+			{
+				// place into standbyQueue’s readyQueue;
+				standbyQueue.pollFirst().ready();		
+			}
+		}
+		KThread.currentThread().yield();
+	}
+
+	/**
+	 * Put the current thread to sleep for at least <i>x</i> ticks,
+	 * waking it up in the timer interrupt handler. The thread must be
+	 * woken up (placed in the scheduler ready set) during the first timer
+	 * interrupt where
+	 *
+	 * <p><blockquote>
+	 * (current time) >= (WaitUntil called time)+(x)
+	 * </blockquote>
+	 *
+	 * @param	x	the minimum number of clock ticks to wait.
+	 *
+	 * @see	nachos.machine.Timer#getTime()
+	 */
+	public void waitUntil(long x) {
+		// for now, cheat just to get something working (busy waiting is bad)
+		
+		// ASSIGN TO PRIVATE VARIABLE INSTANCE TO BE USED WITHIN THE CLASS
+		this.wakeTime = Machine.timer().getTime() + x;
+		
+		// while ( wakeUpTime  > systemCurrentTime){
+		while (wakeTime > Machine.timer().getTime())
+		{
+			
+			// ADD CURRENT THREAD TO standybyQueue
+			standbyQueue.add(KThread.currentThread());
+			KThread.yield();
+			
+			// ?????
+//			KThread.sleep();
+		}
+	}
 }
--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -9,11 +9,27 @@ import nachos.machine.*;
  * be a time when both a speaker and a listener are waiting, because the two
  * threads can be paired off at this point.
  */
+
 public class Communicator {
     /**
      * Allocate a new communicator.
      */
+	
+	
+	private Lock lk;
+	private Condition speaker;
+	private Condition listener;
+	private int word;
+	
     public Communicator() {
+    	// CREATING NEW LOCK 'lk' TO MAKE SPEAK AND LISTEN 
+    	this.lk = new Lock();
+    	// different from pseudo-code: Condition2 vs Condition
+    	
+    	/* this.speaker = new Condition2(lk); */
+    	this.speaker = new Condition(lk);
+    	/* this.listener = new Condition2(lk); */
+    	this.listener = new Condition(lk);
     }
 
     /**
@@ -26,7 +42,28 @@ public class Communicator {
      *
      * @param	word	the integer to transfer.
      */
+    	
+    	
     public void speak(int word) {
+    	
+    	// acquire lock
+    	lk.acquire();
+    	
+    	// while another thread is speaking
+    	while (listen() == 0)
+    	{
+    		// listener sleeps
+    		/* speaker.sleep */
+    		listener.sleep();
+    	}
+    	
+    	// saving the word to current instance of communicator
+    	this.word = word;
+    	listener.wake(); 
+    	speaker.sleep();
+    	
+    	// release lock
+    	lk.release();
     }
 
     /**
@@ -35,7 +72,19 @@ public class Communicator {
      *
      * @return	the integer transferred.
      */    
+    
     public int listen() {
-	return 0;
+    	int returnWord = 0; 
+    	lk.acquire();
+    	while(listen() == 0)
+    	{
+    		listener.sleep();
+    	} 
+    	returnWord = word;
+//    	word = null;   CANNOT SET A INT TO NULL UNLESS USING OPTIONAL VARIABLE LIBRARY
+// 		SEE https://www.tutorialspoint.com/java8/java8_optional_class.htm
+    	speaker.wakeAll();
+    	lk.release();
+	return returnWord;
     }
 }
